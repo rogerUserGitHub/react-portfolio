352f4a7c8461d1b94f3f46abb706262e1dcba5bd
Roger <90182060+rogerUserGitHub@users.noreply.github.com>
2022-12-18 14:48:06 +0100

    fix layoug

--- react-portfolio-app/src/views/homepage/Home.tsx
+++ react-portfolio-app/src/views/homepage/Home.tsx
@@ -157,14 +157,12 @@ export default function Home() {
                 </Item>
               </Stack>
             </Grid>
-            <Grid container>
-              <Grid item xs={12} md={12} lg={12} className='arrow'>
-                <div onClick={handleArrowClick}>
-                  <a>
-                    <img src='/arrow.png'></img>
-                  </a>
-                </div>
-              </Grid>
+            <Grid item xs={12} md={12} lg={12} className='arrow'>
+              <div onClick={handleArrowClick}>
+                <a>
+                  <img src='/arrow.png'></img>
+                </a>
+              </div>
             </Grid>
           </Grid>
         </Container>
