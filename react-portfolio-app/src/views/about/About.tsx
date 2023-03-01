105fb6ad93bda249c3b2aaf9c700d27b94b8e41c
Roger <90182060+rogerUserGitHub@users.noreply.github.com>
2023-03-01 14:02:06 +0100

    fix max width profile picture

--- react-portfolio-app/src/views/about/About.tsx
+++ react-portfolio-app/src/views/about/About.tsx
@@ -6,6 +6,9 @@ export default function About() {
   const ProfilePic = styled('img')({
     width: 'auto',
     height: 'auto',
+    maxWidth: '100%',
+    backgroundSize: 'cover',
+    objectFit: 'cover',
     justifyContent: 'center',
     alignItems: 'center',
     boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.75)',
